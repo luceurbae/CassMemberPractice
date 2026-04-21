7d984217015c3b9c3c25302ff02e111f380c707e
zaluce <145462429+luceurbae@users.noreply.github.com>
2026-04-21 14:52:28 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,13 +25,13 @@ namespace LatihanMemberClass
 
             Console.WriteLine("Origin : {0}", ticket1.Origin);
             Console.WriteLine("Destination : {0}", ticket1.Destination);
-            Console.WriteLine("Cost : {0}", ticket1.Cost);
+            Console.WriteLine("Cost : {0} USD", ticket1.Cost);
             Console.WriteLine("Convert to Rupiah : {0}", ticket1.Cost * 15000);
 
             Console.WriteLine("Origin : {0}", ticket2.Origin);
             Console.WriteLine("Destination : {0}", ticket2.Destination);
-            Console.WriteLine("Cost : {0}", ticket2.Cost);
-            Console.WriteLine("Convert to Rupiah : {0}", ticket2.Cost / 15000);
+            Console.WriteLine("Cost : {0} IDR", ticket2.Cost);
+            Console.WriteLine("Convert to USD : {0}", ticket2.Cost / 15000);
         }
     }
 }
